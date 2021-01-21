58b622c6bcd4c383860a99317595d56e741abf2a
mustankap <you@example.com>
2021-01-21 21:10:01 +0530

    update

--- ocr.py
+++ ocr.py
@@ -1,31 +1,26 @@
 
-DEBUG = False
+
+DEBUG = True
 
 if DEBUG:
-	# This code only exists to help us visually inspect the images.
-	# It's in an `if DEBUG:` block to illustrate that we don't need it for our code to work.
 	from PIL import Image
 	import numpy as np
-
 	def read_image(path):
-		return np.asarray(Image.open(path).convert('L'))
+		return np.asarray(Image.open(path).convert('L'))#bytes to pixel
 	def write_image(image, path):
 		img = Image.fromarray(np.array(image), 'L')
 		img.save(path)
-
+	
 
 DATA_DIR = 'data/'
 TEST_DIR = 'test/'
-
-TEST_DATA_FILENAME = DATA_DIR + '/t10k-images'
-TEST_LABELS_FILENAME = DATA_DIR + '/t10k-labels'
-TRAIN_DATA_FILENAME = DATA_DIR + '/train-images'
-TRAIN_LABELS_FILENAME = DATA_DIR + '/train-labels'
-
+TEST_DATA_FILENAME = DATA_DIR + 't10k-images.idx3-ubyte'
+TEST_LABELS_FILENAME = DATA_DIR + 't10k-labels.idx1-ubyte'
+TRAIN_DATA_FILENAME = DATA_DIR + 'train-images.idx3-ubyte'
+TRAIN_LABELS_FILENAME = DATA_DIR + 'train-labels.idx1-ubyte'
 
 def bytes_to_int(byte_data):
 	return int.from_bytes(byte_data, 'big')
-
 
 def read_images(filename, n_max_images=None):
 	images = []
@@ -46,8 +41,8 @@ def read_images(filename, n_max_images=None):
 				image.append(row)
 			images.append(image)
 		return images
-
-
+		
+		
 def read_labels(filename, n_max_labels=None):
 	labels = []
 	with open(filename, 'rb') as f:
@@ -60,15 +55,13 @@ def read_labels(filename, n_max_labels=None):
 			labels.append(label)
 	return labels
 
-
 def flatten_list(l):
 	return [pixel for sublist in l for pixel in sublist]
 
 
 def extract_features(X):
 	return [flatten_list(sample) for sample in X]
-
-
+	
 def dist(x, y):
 	"""
 	Returns the Euclidean distance between vectors `x` and `y`.
@@ -78,24 +71,20 @@ def dist(x, y):
 			(bytes_to_int(x_i) - bytes_to_int(y_i)) ** 2
 			for x_i, y_i in zip(x, y)
 		]
-	) ** (0.5)
-
+	) ** (0.5)	
+	
 
 def get_training_distances_for_test_sample(X_train, test_sample):
 	return [dist(train_sample, test_sample) for train_sample in X_train]
-
-
+	
 def get_most_frequent_element(l):
 	return max(l, key=l.count)
-
-
+'''funning k nearest neighbours for every sample which we had to find out we are comparing it to all the other digits
+getting the ones which were closest and out them we get the top k and hen we get he most frequen out of those top k'''
 def knn(X_train, y_train, X_test, k=3):
 	y_pred = []
 	for test_sample_idx, test_sample in enumerate(X_test):
-		print(test_sample_idx, end=' ', flush=True)
-		training_distances = get_training_distances_for_test_sample(
-		    X_train, test_sample
-		)
+		training_distances = get_training_distances_for_test_sample(X_train, test_sample)
 		sorted_distance_indices = [
 		pair[0]
 		for pair in sorted(
@@ -103,43 +92,39 @@ def knn(X_train, y_train, X_test, k=3):
 			key=lambda x: x[1]
 		)
 		]
+		
 		candidates = [
-		    y_train[idx]
+		   y_train[idx]
 		    for idx in sorted_distance_indices[:k]
 		]
+		
 		top_candidate = get_most_frequent_element(candidates)
 		y_pred.append(top_candidate)
-		print()
+		
+		
 	return y_pred
-
+	
 def main():
-	'''n_train = 1000
-	n_test = 10
-	k = 7
-	print(f'Dataset: {DATASET}')
-	print(f'n_train: {n_train}')
-	print(f'n_test: {n_test}')
-	print(f'k: {k}')'''
-	X_train = read_images(TRAIN_DATA_FILENAME, 30000)
-	y_train = read_labels(TRAIN_LABELS_FILENAME, 30000)
-	X_test = read_images(TEST_DATA_FILENAME, 300)
-	y_test = read_labels(TEST_LABELS_FILENAME, 300)
-
+	x_train = read_images(TRAIN_DATA_FILENAME,1000)
+	y_train = read_labels(TRAIN_LABELS_FILENAME,1000)
+	x_test = read_images(TEST_DATA_FILENAME,5)
+	y_test = read_labels(TEST_LABELS_FILENAME,5)
+	
 	if DEBUG:
-		# Write some images out just so we can see them visually.
-		for idx, test_sample in enumerate(X_test):
-			write_image(test_sample, f'{TEST_DIR}{idx}.png')
-		# Load in the `our_test.png` we drew ourselves!
-		X_test = [read_image(f'{DATA_DIR}our_test.png')]
-		y_test = [5]
-
-	X_train = extract_features(X_train)
-	X_test = extract_features(X_test)
-
-	y_pred = knn(X_train, y_train, X_test, k)
-
+		for idx, test_sample in enumerate(x_test):
+			write_image(test_sample,f'{TEST_DIR}{idx}.png')
+		#x_test=[read_image(f'{DATA_DIR}our_test.png')]
+		#y_test=[3]
+			
+	x_train = extract_features(x_train)
+	x_test = extract_features(x_test)
+	
+	y_pred = knn(x_train,y_train,x_test,3)#y_pred has a prediction of each of or x_tests
+	
+	print(y_pred)
+	'''comparing every predicted label to every true label'''
 	accuracy = sum([
-	int(y_pred_i == y_test_i)
+	int(y_pred_i ==y_test_i)
 	for y_pred_i, y_test_i
 	in zip(y_pred, y_test)
 	]) / len(y_test)
@@ -147,13 +132,11 @@ def main():
 	print(f'Predicted labels: {y_pred}')
 	print(f'Accuracy: {accuracy * 100}%')
 
-
+	#print(f'point is {bytes_to_int(y_test[test_sample_idx])} and we guessed {candidates}')
+		
+		#y_sample=5
+	
+	
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
 
